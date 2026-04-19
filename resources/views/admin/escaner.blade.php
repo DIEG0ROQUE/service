3c4c9394388f58331e5047618a55de33bf12c9e0
DIEG0ROQUE <diegomrh9@gmail.com>
2026-04-19 00:01:47 -0600

    fix: agregar favicon personalizado

--- resources/views/admin/escaner.blade.php
+++ resources/views/admin/escaner.blade.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>Validación de Tarjetones - ITO</title>
+    <link rel="icon" type="image/x-icon" href="{{ asset('favicon.ico') }}?v={{ time() }}">
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     <script src="https://unpkg.com/html5-qrcode" type="text/javascript"></script>
     <script defer src="https://unpkg.com/alpinejs@3.x.x/dist/cdn.min.js"></script>
