68b68dfcc2bde542437fdd8278824fcd08242017
DIEG0ROQUE <diegomrh9@gmail.com>
2026-04-19 22:55:59 -0600

    fix: usar icon.ico para favicon

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login - ITO</title>
-    <link rel="icon" type="image/x-icon" href="/favicon.ico?v=99">
+    <link rel="icon" type="image/x-icon" href="/icon.ico">
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     <script defer src="https://unpkg.com/alpinejs@3.x.x/dist/cdn.min.js"></script>
 </head>
